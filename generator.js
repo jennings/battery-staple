5953788f968f4e9f6244f80d0494937dcec10a82
Stephen Jennings <stephen.g.jennings@gmail.com>
2014-04-11 16:07:13 -0700

    Initial commit

---
+++ generator.js
@@ -0,0 +1,45 @@
+window.Generator = new function () {
+
+    this.generate = function() {
+        var pw = randomWord() + " " +
+                 randomWord() + " " +
+                 randomWord() + " " +
+                 randomWord();
+
+        return pw;
+    }
+
+    function randomWord() {
+        var max = window.DICT.length;
+        var ix = Math.floor(Math.random() * (max + 1));
+        return window.DICT[ix]
+    }
+
+}();
+
+function addListener(element, eventName, handler) {
+    if (element.addEventListener) {
+        element.addEventListener(eventName, handler, false);
+    } else if (element.attachEvent) {
+        element.attachEvent('on' + eventName, handler);
+    } else {
+        element['on' + eventName] = handler;
+    }
+}
+
+addListener(document.getElementById('generate'), 'click', function () {
+    document.getElementById('pw').value = Generator.generate();
+})
+
+addListener(document.getElementById('copy'), 'click', function () {
+    chrome.permissions.request({
+        permissions: ['clipboardWrite']
+    }, function(granted){
+        if (granted) {
+            document.getElementById('generate').select();
+            document.execCommand('copy');
+        } else {
+            console.log('permission not granted')
+        }
+    })
+})
