7344f467a42933646fc29b7d5c6eabd4142ccd9b
Stephen Jennings <stephen.g.jennings@gmail.com>
2014-08-12 21:40:39 -0700

    Add Gruntfile for running JSHint

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        jshint: {
+            files: ['*.js']
+        },
+        watch: {
+            files: ['*.js'],
+            tasks: ['jshint']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['watch']);
+
+};
